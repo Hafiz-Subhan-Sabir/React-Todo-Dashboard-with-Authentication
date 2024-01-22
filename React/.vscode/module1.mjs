a9eef370750216f89c80b21dac71d1b8ecb4ccd5
Hafiz-Subhan-Sabir <hafizsubhan909@gmail.com>
2024-01-22 16:17:02 +0500

    Adding TextUtils files

---
+++ React/.vscode/module1.mjs
@@ -0,0 +1,7 @@
+const a = "Hafiz";
+const b = "Subhan";
+const c = "Sabir";
+
+export default a;
+export {b};
+export {c};
---
+++ React/.vscode/module2.mjs
@@ -0,0 +1,4 @@
+import ay, {b,c} from './module1.mjs'
+console.log(ay);
+console.log(b);
+console.log(c);
