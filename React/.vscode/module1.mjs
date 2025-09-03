e859cb47babaf12a641700d9efc716d1592dc152
Hafiz-Subhan-Sabir <152702409+Hafiz-Subhan-Sabir@users.noreply.github.com>
2025-09-03 12:48:02 +0500

    Delete React directory
    For updating this

--- React/.vscode/module1.mjs
+++
@@ -1,7 +0,0 @@
-const a = "Hafiz";
-const b = "Subhan";
-const c = "Sabir";
-
-export default a;
-export {b};
-export {c};
--- React/.vscode/module2.mjs
+++
@@ -1,4 +0,0 @@
-import ay, {b,c} from './module1.mjs'
-console.log(ay);
-console.log(b);
-console.log(c);
